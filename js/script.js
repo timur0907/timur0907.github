ea1da9a64b6ad4778f43e3b76eb89955e1b7d9d0
timur0907 <85564897+timur0907@users.noreply.github.com>
2024-06-17 19:55:21 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,8 +12,40 @@ let burger = document.querySelector(".burger");
 let size = document.querySelectorAll(".size");
 
 header_burger.addEventListener("click", () => {
-  header_burger.classList.toggle("active");
-  burger.classList.toggle("active");
+  if (burger.classList[1] != "active") {
+    header_burger.classList.add("active");
+    burger.classList.add("active");
+    document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
+  } else {
+    header_burger.classList.remove("active");
+    burger.classList.remove("active");
+    document.body.style.overflow = "unset";
+    document.documentElement.style.overflow = "unset";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let x = 0;
+  function handleClick(event) {
+    if (event.target.closest(".header__content-burger")) {
+      return;
+    } else {
+      if (burger.classList[1] == "active") {
+        x += 1;
+        if (x == 2) {
+          header_burger.classList.remove("active");
+          burger.classList.remove("active");
+          x = 0;
+          document.body.style.overflow = "unset";
+          document.documentElement.style.overflow = "unset";
+        }
+      }
+    }
+  }
+
+  document.addEventListener("click", handleClick);
+  document.addEventListener("touchstart", handleClick);
 });
 
 size.forEach((element) => {
@@ -57,3 +89,14 @@ cart_back.addEventListener("click", () => {
   cart_content_middle.classList.remove("off");
   cart_content_bottom.classList.remove("active");
 });
+
+
+
+var ua = navigator.userAgent.toLowerCase();
+var is_safari = (ua.indexOf("safari/") > -1 && ua.indexOf("chrome") < 0);
+if (is_safari) {
+  var video = document.getElementById('#video-bg');
+  setTimeout(function () {
+    video.play();
+  }, 50);
+}
